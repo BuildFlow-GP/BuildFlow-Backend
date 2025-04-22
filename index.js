ced6dd25b12f8d4e709c8480bf509ef4948fa96d
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2025-04-22 22:36:57 +0300

    fix problem of cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const authRoutes = require('./routes/userRoute');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
